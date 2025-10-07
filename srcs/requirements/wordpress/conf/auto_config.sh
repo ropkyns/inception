987190075a9e628017e8068729777374d9d3b15c
ropkyns <paulmart@student.42.fr>
2025-10-07 16:32:57 +0200

    omg c'est bon ????

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -5,13 +5,13 @@ until mysql -h mariadb -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SELECT 1" >/dev/n
 done
 echo "MariaDB is up - executing command"
 
-if [ -f /var/www/wordpress/wp-config.php ]; then
+if [ -f /var/www/html/wp-config.php ]; then
 	echo "WordPress already installed"
 else
 		
-	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb:3306 --path='/var/www/wordpress' --skip-check --allow-root
+	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb:3306 --path='/var/www/html' --skip-check --allow-root
 	wp core install \
-		--path='/var/www/wordpress' \
+		--path='/var/www/html' \
 		--url="https://$DOMAIN_NAME" \
 		--title="$WP_TITLE" \
 		--admin_user="$WP_USER" \
@@ -19,17 +19,17 @@ else
 		--admin_email="$WP_EMAIL" \
 		--skip-email \
 		--allow-root
-	wp user create testo test@tes.to --role=author --path='/var/www/wordpress' --user_pass=testo --allow-root
+	wp user create testo test@tes.to --role=author --path='/var/www/html' --user_pass=testo --allow-root
 fi
 
-if wp user get admin --path='/var/www/wordpress' --allow-root > /dev/null 2>&1; then
+if wp user get admin --path='/var/www/html' --allow-root > /dev/null 2>&1; then
 	echo "Deleting user 'Admin'"
-	wp user delete admin --path='/var/www/wordpress' --allow-root --yes
+	wp user delete admin --path='/var/www/html' --allow-root --yes
 fi
 
-if ! wp user get "$WP_USER" --path='/var/www/wordpress' --allow-root > /dev/null 2>&1; then
+if ! wp user get "$WP_USER" --path='/var/www/html' --allow-root > /dev/null 2>&1; then
 	echo "Creating admin user '$WP_USER'"
-	wp user create "$WP_USER" "$WP_EMAIL" --role=administrator --user_pass="$WP_PASSWORD" --path='/var/www/wordpress' --allow-root
+	wp user create "$WP_USER" "$WP_EMAIL" --role=administrator --user_pass="$WP_PASSWORD" --path='/var/www/html' --allow-root
 else
 	echo "Admin user $WP_USER already exists."
 fi
