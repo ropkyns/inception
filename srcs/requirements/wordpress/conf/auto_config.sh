4c34d74516b4dbdaa51152a4484fc622ac73b794
ropkyns <paulmart@student.42.fr>
2025-10-08 15:24:41 +0200

    ALORS PEUT ETRE

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -1,17 +1,24 @@
 echo "Waiting for Mariadb to be ready..."
-until mysql -h mariadb -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SELECT 1" >/dev/null 2>&1; do
+until mysqladmin ping -h mariadb --silent; do
 	echo "Mariadb is unvailable - sleeping"
-	sleep 2
+	sleep 3
 done
 echo "MariaDB is up - executing command"
 
-if [ -f /var/www/html/wp-config.php ]; then
+if [ -f "/var/www/html/wp-config.php" ]; then
 	echo "WordPress already installed"
 else
-		
-	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb:3306 --path='/var/www/html' --skip-check --allow-root
+	if [ ! -x /usr/local/bin/wp ]; then
+		echo "wordpress cli installation"
+		wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O /usr/local/bin/wp
+		chmod +x /usr/local/bin/wp
+
+	fi
+
+	wp core download --path=/var/www/html --allow-root
+	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb:3306 --path=/var/www/html --skip-check --allow-root
 	wp core install \
-		--path='/var/www/html' \
+		--path=/var/www/html \
 		--url="https://$DOMAIN_NAME" \
 		--title="$WP_TITLE" \
 		--admin_user="$WP_USER" \
@@ -19,17 +26,17 @@ else
 		--admin_email="$WP_EMAIL" \
 		--skip-email \
 		--allow-root
-	wp user create testo test@tes.to --role=author --path='/var/www/html' --user_pass=testo --allow-root
+	# wp user create testo test@tes.to --role=author --path=/var/www/html --user_pass=testo --allow-root
 fi
 
-if wp user get admin --path='/var/www/html' --allow-root > /dev/null 2>&1; then
+if wp user get admin --path=/var/www/html --allow-root > /dev/null 2>&1; then
 	echo "Deleting user 'Admin'"
-	wp user delete admin --path='/var/www/html' --allow-root --yes
+	wp user delete admin --path=/var/www/html --allow-root --yes
 fi
 
-if ! wp user get "$WP_USER" --path='/var/www/html' --allow-root > /dev/null 2>&1; then
+if ! wp user get "$WP_USER" --path=/var/www/html --allow-root > /dev/null 2>&1; then
 	echo "Creating admin user '$WP_USER'"
-	wp user create "$WP_USER" "$WP_EMAIL" --role=administrator --user_pass="$WP_PASSWORD" --path='/var/www/html' --allow-root
+	wp user create "$WP_USER" "$WP_EMAIL" --role=administrator --user_pass="$WP_PASSWORD" --path=/var/www/html --allow-root
 else
 	echo "Admin user $WP_USER already exists."
 fi
